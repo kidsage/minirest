06f2e87bd23f7bf7200d1d4d5373ed46dfbf6b25
kidsage <ruhyun92@gmail.com>
2022-07-08 23:17:35 +0900

    7/8 3rt

--- account/views.py
+++ account/views.py
@@ -2,10 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from account.models import HelloWorld
+
 # Create your views here.
 
 def hello_world(request):
+
     if request.method == 'POST':
-        return render(request, 'account/hello_world.html', context={'text':'post method'})
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        
+        return render(request, 'account/hello_world.html', context={'hello_world_output':new_hello_world})
     else:
         return render(request, 'account/hello_world.html', context={'text':'get method'})
