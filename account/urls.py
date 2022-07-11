74d4a2cb56562d359c3baec72adf33487892756e
kidsage <ruhyun92@gmail.com>
2022-07-11 16:01:40 +0900

    7/11 2nd

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from account.views import AccountCreateView, hello_world
+from account.views import AccountCreateView, AccountDetailView, hello_world
 from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = "account"
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),
     path('login/', LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),  # primary key 정보를 받기 위해 뒤에 int:pk 붙여줌
 ]
