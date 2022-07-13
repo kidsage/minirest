e1fc5744e243fd345f1e940c312daa2462a5d4db
kidsage <ruhyun92@gmail.com>
2022-07-14 00:31:03 +0900

    7/14 article update

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True) # 회원 탈퇴를 했을 때 게시글이 사라지지 않고 주인 없는 게시글로 남음
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+    created_at = models.DateField(auto_created=True, null=True)
