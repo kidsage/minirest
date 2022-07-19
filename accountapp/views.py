11a8fd74f25d1a9db343f0f86e002dfab4eb2d4c
kidsage <ruhyun92@gmail.com>
2022-07-19 23:42:45 +0900

    7/19 multipleobjectmixin add

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirec
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from accountapp.decorators import account_ownership_required
@@ -10,6 +11,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 from accountapp.models import HelloWorld
+from articleapp.models import Article
 
 # Create your views here.
 
@@ -37,10 +39,17 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
+
 
 
 @method_decorator(has_ownership, 'get')
--- articleapp/models.py
+++ articleapp/models.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from projectapp.models import Project
+
 
 # Create your models here.
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True) # 회원 탈퇴를 했을 때 게시글이 사라지지 않고 주인 없는 게시글로 남음
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='project', null=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=False)
     content = models.TextField(null=True)
