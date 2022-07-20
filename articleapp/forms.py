1fd425c1b79d13e62b8001e7011811c880227848
kidsage <ruhyun92@gmail.com>
2022-07-21 02:30:11 +0900

    7/21 article fix

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,17 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class' : 'editable text-start',
+                                                           'style' : 'height: auto'}))
+
+    # modelchoicefield는 queryset이 필수
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False) 
+
     class Meta:
         model = Article
         fields = ['project', 'title', 'image', 'content']
