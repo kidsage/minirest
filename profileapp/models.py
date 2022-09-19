2fdeece0063493aa5c14c52096dfbcccb5c227b2
kidsage <ruhyun92@gmail.com>
2022-09-19 20:36:45 +0900

    9/19 fix account, profile

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,6 +1,5 @@
 from accountapp.models import User
 from django.db import models
-from django.utils.html import mark_safe
 
 # Create your models here.
 
--- profileapp/views.py
+++ profileapp/views.py
@@ -4,16 +4,15 @@ from django.views.generic import CreateView, UpdateView
 from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from django.utils.decorators import method_decorator
-
 from profileapp.models import Profile
+from accountapp.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
 class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    # success_url = reverse_lazy('accountapp:detail') # pk가 붙은 url은 따로 메소드를 지정해줘야 인자를 받을 수 있다
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
